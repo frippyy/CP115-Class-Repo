769899d72855927fbed840b8b8eeb96bc44917bd
frippyy <frippy.email@gmail.com>
2026-07-07 11:04:04 +0800

    Test exercise 3

--- labs/lab04/exercise3/lab-4-3.py
+++ labs/lab04/exercise3/lab-4-3.py
@@ -1,9 +1,9 @@
 hours = int(input())
 if hours > 5:
-    parkingFee = hours - 5 * 3 + 6
+    parkingFee = ((hours - 5) * 3) + 6
 else:
     if hours > 2:
-        parkingFee = hours - 2 * 2
+        parkingFee = (hours - 2) * 2
     else:
         parkingFee = 0
 if parkingFee > 30:
