4a28f49d4fffabdb1376e3b4e181789fba09801e
frippyy <frippy.email@gmail.com>
2026-07-07 11:16:12 +0800

    Test exercise 1

--- labs/lab04/exercise1/lab-4-1.py
+++ labs/lab04/exercise1/lab-4-1.py
@@ -1,10 +1,9 @@
 kwh = int(input())
 if kwh > 200:
-    rate = 0.75
+    totalBill = 80 + ((kwh - 200) * 0.75)
 else:
     if kwh > 100:
-        rate = 0.5
+        totalBill = 30 + ((kwh - 100) * 0.5)
     else:
-        rate = 0.3
-totalBill = kwh * rate
+        totalBill = kwh * 0.3
 print(totalBill)
