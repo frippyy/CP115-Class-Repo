4b2f8acfa725a218f6e79d71ba345cae11b481e6
frippyy <frippy.email@gmail.com>
2026-09-01 11:29:32 +0800

    Lab 07

--- labs/lab07/exercise3/exercise3.py
+++ labs/lab07/exercise3/exercise3.py
@@ -3,7 +3,17 @@ price = float(input())
 quantity = int(input())
 member_answer = input()
 
+order_total = price * quantity
 
+if order_total >= 100:
+    free_shipping = True
+else:
+    free_shipping = False
+
+if member_answer == "yes":
+    is_member = True
+else:
+    is_member = False
 
 print(name.upper())
 print(order_total)
--- labs/lab07/exercise5/exercise5.py
+++ labs/lab07/exercise5/exercise5.py
@@ -3,7 +3,15 @@ password = input()
 origin = input()
 destination = input()
 
+username = name.lower()
+name_length = len(name)
 
+if len(password) >= 8:
+    long_enough = True
+else:
+    long_enough = False
+
+route = origin.upper() + "-" + destination.upper()
 
 print(username)
 print(name_length)
